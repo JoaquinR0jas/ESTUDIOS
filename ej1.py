a5c7c29bf6bdc071cdfa7dd4a2976365aff728cf
JavierJara95 <jav.jarag@duocuc.cl>
2025-06-23 18:00:08 -0400

    Create ej1.py

---
+++ ej1.py
@@ -0,0 +1,31 @@
+# Desarrolla un programa para una tienda de electronica que permita gestionar clientes
+# El menu principal debe mostrar:
+# SISTEMA DE GESTION DE CLIENTES
+# 1 Registrar nuevo cliente
+# 2 buscar cliente por id
+# 3 mostrar todos los clientes
+# 4 eliminar cliente
+# 5 salir
+# Todas las opciones del menu deben estar implementadas mediante funciones separadas
+
+# Opcion 1 Registrar nuevo cliente
+# permite ingresar ID del cliente (numero), nombre(string) y email (string)
+# el ID del cliente no debe estar repetido
+# El email debe contener un @ y al menos un . (validacion simple)
+# Si los datos son validos, el cliente se registra exitosamente
+
+
+# Opcion 2 Buscar cliente por ID 
+# Pide el ID del cliente a buscar
+# SI el cliente existe, muestra toda su informacion. SI no, indica que no se encontró
+
+
+# OPcion 3 Mostrar todos los clientes
+# Lista la informacion de todos los clientes registrados. Si no hay clientes, muestra un mensaje
+
+# Opcion 4: ELiminar cliente
+# Pide el ID del cliente a eliminar. Si existe, lo elimina y confirma. SI no, indica que no se encontro
+
+# Opcion 5: Salir
+# EL programa debe terminar mostrando: "Saliendo del sistema de gestion de clientes..."
+# Si se ingresa una opcion distinta debe mostrar: "Opcion no valida. Por favor, ingrese un numero entre 1 y 5"
